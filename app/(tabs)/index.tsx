bc2c41fa0ca3566ae006255eb2991fc6fd862624
Antoine NAVETTE <antoinenavette@gmail.com>
2025-03-11 12:23:22 +0100

    feat: search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,25 @@
 import { Pressable, Text, TextInput, View } from 'react-native';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { fetch } from 'expo/fetch';
+import { Image } from 'expo-image';
 
 const HomePage = () => {
     const [pokemonName, setPokemonName] = useState('');
-    const [results, setResults] = useState(null);
+    const [results, setResults] = useState<{ id: number; name: string; image: string } | null>(null);
 
-    const handleSearch = useCallback(() => {
-        
+    const handleSearch = useCallback(async () => {
+        if (!pokemonName) return;
+
+        const response = await fetch('https://pokebuildapi.fr/api/v1/pokemon/' + pokemonName.toLowerCase(), {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) return;
+
+        const pokemon = await response.json();
+        setResults(pokemon);
     }, [pokemonName]);
 
     return (
@@ -17,9 +30,17 @@ const HomePage = () => {
                 placeholder="Entrez le nom du pokémon"
                 onChangeText={setPokemonName}
             />
-            <Pressable className="bg-red-600 px-10 py-3 rounded-md mt-5">
+            <Pressable onPress={handleSearch} className="bg-red-600 px-10 py-3 rounded-md mt-5">
                 <Text className="text-white fond-bold">Rechercher</Text>
             </Pressable>
+            {results && (
+                <View>
+                    <Image source={{ uri: results.image }} style={{ width: 100, height: 100 }} />
+                    <Text className="text-2xl font-bold">
+                        {results.name} {results.id}
+                    </Text>
+                </View>
+            )}
         </View>
     );
 };
