6f80894be92312044fcae452c89c3150058b987c
Antoine NAVETTE <antoinenavette@gmail.com>
2025-03-11 11:52:28 +0100

    feat: search page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,13 @@
-import { Text, TextInput, View } from 'react-native';
-import React, { useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const HomePage = () => {
     const [pokemonName, setPokemonName] = useState('');
+    const [results, setResults] = useState(null);
+
+    const handleSearch = useCallback(() => {
+        
+    }, [pokemonName]);
 
     return (
         <View className="flex items-center justify-center h-screen">
@@ -10,7 +15,11 @@ const HomePage = () => {
             <TextInput
                 className="bg-white rounded-md border border-gray-200 px-10 py-3"
                 placeholder="Entrez le nom du pokémon"
+                onChangeText={setPokemonName}
             />
+            <Pressable className="bg-red-600 px-10 py-3 rounded-md mt-5">
+                <Text className="text-white fond-bold">Rechercher</Text>
+            </Pressable>
         </View>
     );
 };
