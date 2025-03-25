7a3ab69bb0cf0a26afc9531a0934f85a07ab2991
Antoine-NVE <antoinenavette@mail.com>
2025-03-25 10:12:36 +0100

    feat: detail page

--- app/(tabs)/pokemon/[id].tsx
+++ app/(tabs)/pokemon/[id].tsx
@@ -1,16 +1,34 @@
+import { Image } from 'expo-image';
 import { Link, useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, ScrollView } from 'react-native';
 
 const Pokemon = () => {
     const { id } = useLocalSearchParams();
+    const [pokemon, setPokemon] = useState<{ name: string; image: string } | null>(null);
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const response = await fetch(`https://pokebuildapi.fr/api/v1/pokemon/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const pokemon = await response.json();
+
+            setPokemon(pokemon);
+        };
+
+        fetchPokemon();
+    }, []);
 
     return (
-        <View>
-            <Text>Pokemon Page {id}</Text>
-
+        <ScrollView>
+            <Image source={{ uri: pokemon?.image }} style={{ width: 100, height: 100 }} />
+            <Text>{pokemon?.name}</Text>
+            <Text>{JSON.stringify(pokemon, null, 3)}</Text>
             <Link href="/">Retourner à l'accueil</Link>
-        </View>
+        </ScrollView>
     );
 };
 
