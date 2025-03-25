b200c8f89cdd95d4ee9e124b2b2c825349ca6966
Antoine-NVE <antoinenavette@mail.com>
2025-03-25 11:10:52 +0100

    feat: link to evolutions and pre-evolution

--- app/(tabs)/pokemon/[id].tsx
+++ app/(tabs)/pokemon/[id].tsx
@@ -5,7 +5,12 @@ import { Text, View, ScrollView } from 'react-native';
 
 const Pokemon = () => {
     const { id } = useLocalSearchParams();
-    const [pokemon, setPokemon] = useState<{ name: string; image: string } | null>(null);
+    const [pokemon, setPokemon] = useState<{
+        name: string;
+        image: string;
+        apiPreEvolution: { name: string; pokedexIdd: number };
+        apiEvolutions: { name: string; pokedexId: number }[];
+    } | null>(null);
 
     useEffect(() => {
         const fetchPokemon = async () => {
@@ -19,15 +24,32 @@ const Pokemon = () => {
         };
 
         fetchPokemon();
-    }, []);
+    }, [id]);
 
     return (
-        <ScrollView>
-            <Image source={{ uri: pokemon?.image }} style={{ width: 100, height: 100 }} />
-            <Text>{pokemon?.name}</Text>
-            <Text>{JSON.stringify(pokemon, null, 3)}</Text>
+        <View className="flex flex-col items-center mt-20">
+            <View className="bg-white rounded-full p-10 flex items-center justify-center">
+                <Image source={{ uri: pokemon?.image }} style={{ width: 100, height: 100 }} />
+            </View>
+            <Text className="text-3xl font-bold">{pokemon?.name}</Text>
+            <View className="flex flex-row w-full">
+                {pokemon?.apiPreEvolution?.pokedexIdd && (
+                    <Link
+                        className="rounded-lg p-5 bg-white shadow-lg flex-1 text-center"
+                        href={`/(tabs)/pokemon/${pokemon?.apiPreEvolution.pokedexIdd}`}>
+                        Voir la pré-évolution
+                    </Link>
+                )}
+                {pokemon?.apiEvolutions && pokemon?.apiEvolutions?.length > 0 && (
+                    <Link
+                        className="rounded-lg p-5 bg-white shadow-lg flex-1 text-center"
+                        href={`/(tabs)/pokemon/${pokemon?.apiEvolutions[0]?.pokedexId}`}>
+                        Voir l'évolution
+                    </Link>
+                )}
+            </View>
             <Link href="/">Retourner à l'accueil</Link>
-        </ScrollView>
+        </View>
     );
 };
 
